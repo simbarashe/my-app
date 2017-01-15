9bfebedc1d3c22aa9b02f57cfac5e74102d1b9a8
Simba Sithole <simba.sithole07@gmail.com>
2017-01-16 00:20:09 +0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,27 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { SiteHeaderComponent } from './site-header/site-header.component';
+import { AdminLinkDirective } from './admin-link.directive';
+import { AdminComponent } from './admin/admin.component';
+import { ConvertToEuroPipe } from './convert-to-euro.pipe';
+
+import { BackendApiService } from './backend-api.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SiteHeaderComponent,
+    AdminLinkDirective,
+    AdminComponent,
+    ConvertToEuroPipe
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [BackendApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
